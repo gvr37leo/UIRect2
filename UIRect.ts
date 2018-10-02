a5caaeaaaa55d099e69d45adde45c21acea89c7f
Paul Boon <gvr37.p.l.boon@gmail.com>
2018-10-02 23:43:22 +0200

    p

--- UIRect.ts
+++ UIRect.ts
@@ -21,29 +21,48 @@ class DragRect{
     topRight:Handle
     bottomLeft:Handle
     bottomRight:Handle
+    handles: Handle[];
 
     constructor(public rect:Rect){
-        this.topLeft = new Handle(new Box(this.rect.getPoint(new Vector(0,0))))
-        this.topRight = new Handle(new Box(this.rect.getPoint(new Vector(1,0))))
-        this.bottomLeft = new Handle(new Box(this.rect.getPoint(new Vector(0,1))))
-        this.bottomRight = new Handle(new Box(this.rect.getPoint(new Vector(1,1))))
+        this.topLeft = new Handle(new Box(new Vector(0,0)))
+        this.topRight = new Handle(new Box(new Vector(0,0)))
+        this.bottomRight = new Handle(new Box(new Vector(0,0)))
+        this.bottomLeft = new Handle(new Box(new Vector(0,0)))
+        this.updateHandles()
+        this.handles = [this.topLeft,this.topRight,this.bottomRight,this.bottomLeft]
 
         this.topLeft.pos.onchange.listen(v => {
             this.rect.min.overwrite(v)
+            this.updateHandles()
         })
 
         this.topRight.pos.onchange.listen(v => {
             this.rect.min.y = v.y
             this.rect.max.x = v.x
+            this.updateHandles()
         })
 
         this.bottomLeft.pos.onchange.listen(v => {
-            this.rect.max.overwrite(v)
+            this.rect.min.x = v.x
+            this.rect.max.y = v.y
+            this.updateHandles()
         })
 
         this.bottomRight.pos.onchange.listen(v => {
-            this.rect.min.x = v.x
-            this.rect.max.y = v.y
+            this.rect.max.overwrite(v)
+            this.updateHandles()
         })
+    }
+
+    updateHandles(){
+        this.topLeft.pos.value.overwrite(this.rect.getPoint(new Vector(0,0)))
+        this.topRight.pos.value.overwrite(this.rect.getPoint(new Vector(1,0)))
+        this.bottomRight.pos.value.overwrite(this.rect.getPoint(new Vector(1,1)))
+        this.bottomLeft.pos.value.overwrite(this.rect.getPoint(new Vector(0,1)))
+    }
+
+    draw(ctxt:CanvasRenderingContext2D){
+        this.handles.forEach(h => h.draw(ctxt))
+        this.rect.draw(ctxt)
     }
 }
--- main.ts
+++ main.ts
@@ -10,8 +10,10 @@ var crret = createCanvas(500,500)
 var canvas = crret.canvas
 var ctxt = crret.ctxt
 
+var dragrect = new DragRect(new Rect(new Vector(10,10), new Vector(100,100)))
+
 loop((dt) => {
     ctxt.clearRect(0,0,500,500)
 
-    ctxt.fillRect(10,10,10,10)
+    dragrect.draw(ctxt)
 })
